d9c38bb916ffeb8362308fc8320f7f0fcd29cd67
Xyrsto <rodrigtomas@gmail.com>
2023-05-03 13:43:53 +0100

    Patch 0.1 -> a TopBar está feita

--- src/app/TopBar.js
+++ src/app/TopBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 class TopBar extends Component
 {
     render()
@@ -7,13 +8,12 @@ class TopBar extends Component
         return(
             <div class = "fixed-top container-fluid bg-dark">
                 <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-                    <div class = "d-inline"></div>
-                    <div class="h1 pl-4 text-light">Show Spot</div>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="h1 pl-4 text-light ">Show Spot</div>
+                    <div className = "TopBar" class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNavAltMarkup">
                         <div class="navbar-nav">
-                            <div class="nav-item nav-link active display-1 text-light">Movies</div>
-                            <div class="nav-item nav-link display-1 text-light">Series</div>
-                            <div class="nav-item nav-link display-1 text-light">Account</div>
+                            <div class="nav-item nav-link active display-1 text-light pd-2">Movies</div>
+                            <div class="nav-item nav-link display-1 text-light pd-2">Series</div>
+                            <div class="nav-item nav-link display-1 text-light pd-2">Account</div>
                         </div>
                     </div>
                 </nav>
