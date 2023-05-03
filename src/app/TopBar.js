0644f15a2f0872fc430e5c61d3237da5a6655ccb
RBMGCardoso <rubenbmgcardoso@gmail.com>
2023-05-03 19:29:41 +0100

    Acertos topbar

--- src/app/TopBar.js
+++ src/app/TopBar.js
@@ -8,19 +8,23 @@ class TopBar extends Component
     {
         return(
             <div>
-                <div class = "fixed-top container-fluid bg-dark bg-gradient">
-                    <nav class="navbar navbar-expand-lg navbar-light bg-dark bg-gradient">
-                        <div class="h1 pl-4 text-light ">Show Spot</div>
-                        <div class="collapse navbar-collapse d-flex flex-row-reverse">
-                            <div class="navbar-nav">
-                                <div class="nav-item nav-link active display-1 text-light pd-2">Movies</div>
-                                <div class="nav-item nav-link display-1 text-light pd-2">Series</div>
-                                <div class="nav-item nav-link display-1 text-light pd-2">Account</div>
+                <div class = "fixed-top container-fluid bg-dark bg-gradient p-0">
+                    <nav class="navbar navbar-expand-lg navbar-light bg-dark bg-gradient d-flex">
+                        <div class="row w-100">
+                            <div class="col-2 ps-4 align-self-center">
+                                <div class="row">
+                                    <div class="col-4 p-2 m-1 small">Movies</div>
+                                    <div class="col-4 p-2 m-1 small border-start">Series</div>  
+                                </div>
+                                
                             </div>
+                            <div class="col align-self-center">ShowSpot</div>
+                            <div class="col-2 pe-4 text-end small align-self-center">Account</div>
                         </div>
                     </nav>
+                    <div class = "TopBar">asasd</div>  
                 </div>   
-                <div class = "TopBar">asdasd</div>  
+                
             </div>
                            
         )
