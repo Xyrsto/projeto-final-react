18ef3b3d53795a553dae5d5f1ddda5b25ad4554e
Xyrsto <rodrigtomas@gmail.com>
2023-05-03 12:58:47 +0100

    TopBar foi criada mas não acabada.

---
+++ src/app/TopBar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class TopBar extends Component
+{
+    render()
+    {
+        return(
+            <div class = "fixed-top container-fluid bg-dark">
+                <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+                    <div class = "d-inline"></div>
+                    <div class="h1 pl-4 text-light">Show Spot</div>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <div class="nav-item nav-link active display-1 text-light">Movies</div>
+                            <div class="nav-item nav-link display-1 text-light">Series</div>
+                            <div class="nav-item nav-link display-1 text-light">Account</div>
+                        </div>
+                    </div>
+
+                </nav>
+            </div>          
+        )
+    }
+}
+export default TopBar;
