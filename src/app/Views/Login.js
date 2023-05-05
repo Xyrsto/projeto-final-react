08c538c4a3aab32623a234cb577732a56ae9560c
Xyrsto <rodrigtomas@gmail.com>
2023-05-05 11:37:38 +0100

    css changes 3

--- src/app/Views/Login.js
+++ src/app/Views/Login.js
@@ -10,17 +10,19 @@ class Login extends Component
             <div class = "fixed-top container-fluid  p-0">
                 <TopBar/>
                 <div class = "loginScreen">
-                    <h1 class = "loginRegisterFont">Login</h1>
-                    <div class = "form-group pt-5">
-                        <h3 class = "loginRegisterFont">Email address</h3>    
-                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"></input>                    
+                    <div class = "loginBox">
+                        <h1 class = "loginRegisterFont">Login</h1>
+                        <div class = "form-group pt-5">
+                            <h3 class = "loginRegisterFont">Email address</h3>    
+                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"></input>                    
+                        </div>
+                        <div class = "form-group pt-5 pb-5">
+                            <h3 class = "loginRegisterFont">Password</h3>    
+                            <input type="email" class="form-control" id="password" aria-describedby="emailHelp" placeholder="Enter password"></input>                    
+                        </div>
+                        <button type="submit" class="btn btn-secondary">Login!</button>
                     </div>
-                    <div class = "form-group pt-5 pb-5">
-                        <h3 class = "loginRegisterFont">Password</h3>    
-                        <input type="email" class="form-control" id="password" aria-describedby="emailHelp" placeholder="Enter password"></input>                    
-                    </div>
-                    <button type="submit" class="btn btn-secondary">Login!</button>
-                </div>
+                </div>                  
             </div>
         )
     }
--- src/app/Views/Register.js
+++ src/app/Views/Register.js
@@ -10,21 +10,24 @@ class Register extends Component
             <div class = "fixed-top container-fluid  p-0">
                 <TopBar/>
                 <div class = "loginScreen">
-                    <h1 class = "loginRegisterFont">Register</h1>
-                    <div class = "form-group pt-5">
-                        <h3 class = "loginRegisterFont">Username</h3>    
-                        <input type="text" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter username"></input>                    
+                    <div class = "loginBox">
+                        <h1 class = "loginRegisterFont">Register</h1>
+                        <div class = "form-group pt-5">
+                            <h3 class = "loginRegisterFont">Username</h3>    
+                            <input type="text" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter username"></input>                    
+                        </div>
+                        <div class = "form-group pt-5">
+                            <h3 class = "loginRegisterFont">Email address</h3>    
+                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"></input>                    
+                        </div>
+                        <div class = "form-group pt-5 pb-5">
+                            <h3 class = "loginRegisterFont">Password</h3>    
+                            <input type="password" class="form-control" id="password" aria-describedby="emailHelp" placeholder="Enter password"></input>                    
+                        </div>
+                        <button type="submit" class="btn btn-secondary">Register!</button>
                     </div>
-                    <div class = "form-group pt-5">
-                        <h3 class = "loginRegisterFont">Email address</h3>    
-                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"></input>                    
-                    </div>
-                    <div class = "form-group pt-5 pb-5">
-                        <h3 class = "loginRegisterFont">Password</h3>    
-                        <input type="password" class="form-control" id="password" aria-describedby="emailHelp" placeholder="Enter password"></input>                    
-                    </div>
-                    <button type="submit" class="btn btn-secondary">Register!</button>
                 </div>
+                    
             </div>
         )
     }
